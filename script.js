56c5e3c9fc732488f3887a7386b9ac7aa7d3deb1
Dwayne Reed <dreedjr1992@gmail.com>
2022-07-26 18:43:04 -0500

    second commit

--- script.js
+++ script.js
@@ -1,1 +1,29 @@
 // TODO: add code here
+window.addEventListener("load", function () {
+  fetch("https://handlers.education.launchcode.org/static/astronauts.json")
+    .then((response) => {
+      return response.json();
+    })
+    .then((json) => {
+      const containder = this.document.getElementById("container");
+      let astronautHTML = "";
+
+      for (astronaut of json) {
+        astronautHTML += `
+              <div class="astronaut">
+                    <div class="bio">
+                    <h3>${astronaut.firstName} ${astronaut.lastName}</h3>
+                    <ul>
+                        <li>Hours in space: ${astronaut['hoursInSpace']}</li>
+                          <li>Active: ${astronaut.active}</li>
+                          <li>Skills: ${astronaut.skills.join(', ')}
+              
+                  </ul>
+               </div>
+               <img class="avatar" src=${astronaut.picture}>
+              </div>
+              `;
+      }
+      container.innerHTML = astronautHTML;
+    });
+});
